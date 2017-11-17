d3df2f9e006cdbf93a1bd587708058501155ce75
Michael R.|SchlaumeierTV <michael@rittmeister.in>
2017-11-17 17:23:14 +0100

    Update

--- src/main/java/de/rubicon/core/Main.java
+++ src/main/java/de/rubicon/core/Main.java
@@ -2,11 +2,14 @@ package de.rubicon.core;
 
 import de.rubicon.util.Configuration;
 import de.rubicon.util.Info;
+import de.rubicon.util.MySQL;
+
 import java.io.File;
 
 public class Main {
 
     private static DiscordCore discordCore;
+    private static MySQL mySQL;
 
     public static Configuration getConfiguration() {
         return configuration;
@@ -16,6 +19,8 @@ public class Main {
 
     public static void main(String[] args) {
         configuration = new Configuration(new File(Info.CONFIG_FILE));
+        mySQL = new MySQL(Info.MYSQL_HOST, Info.MYSQL_PORT, Info.MYSQL_USER, Info.MYSQL_PASSWORD, Info.MYSQL_DATABASE);
+        mySQL.connect();
         discordCore = new DiscordCore();
         discordCore.start();
     }
@@ -24,4 +29,7 @@ public class Main {
         return discordCore;
     }
 
+    public static MySQL getMySQL() {
+        return mySQL;
+    }
 }
