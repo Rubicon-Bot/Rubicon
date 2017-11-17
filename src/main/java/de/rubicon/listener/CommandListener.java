937acf67dfd932eba33141bc26113a1cded6d41b
Michael R.|SchlaumeierTV <michael@rittmeister.in>
2017-11-17 20:46:24 +0100

    MySQL update
    - Improved MySQL class

--- src/main/java/de/rubicon/listener/CommandListener.java
+++ src/main/java/de/rubicon/listener/CommandListener.java
@@ -1,6 +1,7 @@
 package de.rubicon.listener;
 
 import de.rubicon.command.CommandHandler;
+import de.rubicon.core.Main;
 import de.rubicon.util.Info;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -9,7 +10,7 @@ public class CommandListener extends ListenerAdapter {
 
     public void onMessageReceived(MessageReceivedEvent e) {
         try {
-            if (e.getAuthor() != e.getJDA().getSelfUser()) {
+            if (e.getAuthor() != e.getJDA().getSelfUser() && !e.getAuthor().isBot()) {
                 if (e.getMessage().getContent().toLowerCase().startsWith(Info.BOT_DEFAULT_PREFIX.toLowerCase())) {
                     CommandHandler.handleCommand(CommandHandler.parser.parse(e.getMessage().getContent(), e));
                 }
