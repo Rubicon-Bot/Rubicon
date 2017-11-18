8065e17ac5d733644771f0a43d7c3f9250828bbc
ForYaSee <foryasee@gmail.com>
2017-11-18 16:31:04 +0100

    GameAnimator

---
+++ src/main/java/fun/rubicon/core/GameAnimator.java
@@ -0,0 +1,56 @@
+package fun.rubicon.core;
+
+import fun.rubicon.util.Info;
+import fun.rubicon.util.Logger;
+import net.dv8tion.jda.core.entities.Game;
+
+public class GameAnimator {
+
+    private static Thread t;
+    private static boolean running = false;
+
+    private static int currentGame = 0;
+
+    private static String[] gameAnimations = {
+            "Running on " + DiscordCore.getJDA().getGuilds().size() + " servers!",
+            "Helping " + DiscordCore.getJDA().getUsers().size() + " users!",
+            "JDA squad!",
+            Info.BOT_NAME + " " + Info.BOT_VERSION,
+            "Crazy Shit!",
+            "Generating new features...",
+    };
+
+    public static synchronized void start() {
+        if (!running) {
+            t = new Thread(() -> {
+                Logger.debug("Executed");
+                long last = 0;
+                while (running) {
+                    if (System.currentTimeMillis() >= last + 60000) {
+                        DiscordCore.getJDA().getPresence().setGame(Game.of(gameAnimations[currentGame]));
+                        last = System.currentTimeMillis();
+
+                        if (currentGame == gameAnimations.length - 1)
+                            currentGame = 0;
+                        else
+                            currentGame += 1;
+                    }
+                }
+            });
+            t.setName("GameAnimator Thread");
+            running = true;
+            t.start();
+        }
+    }
+
+    public static synchronized void stop() {
+        if (running) {
+            try {
+                running = false;
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
