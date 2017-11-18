e6e848d51980d1fa33aa3c68758223f264a26c10
ForYaSee <foryasee@gmail.com>
2017-11-18 15:40:22 +0100

    Fix

--- src/main/java/de/rubicon/listener/GuildJoinListener.java
+++ src/main/java/de/rubicon/listener/GuildJoinListener.java
@@ -1,7 +1,5 @@
 package de.rubicon.listener;
 
-import fun.rubicon.core.Main;
-import fun.rubicon.util.MySQL;
 import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
@@ -9,7 +7,5 @@ public class GuildJoinListener extends ListenerAdapter{
 
     @Override
     public void onGuildJoin(GuildJoinEvent event) {
-        MySQL mySQL = Main.getMySQL();
-        mySQL.generatePermissions(event.getGuild());
     }
 }
