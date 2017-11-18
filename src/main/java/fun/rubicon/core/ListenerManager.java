6a9c5276586508c263d9c1e713d6c7f485cc2668
ForYaSee <foryasee@gmail.com>
2017-11-18 14:34:31 +0100

    Package Fix

--- src/main/java/fun/rubicon/core/ListenerManager.java
+++ src/main/java/fun/rubicon/core/ListenerManager.java
@@ -1,14 +1,9 @@
 package fun.rubicon.core;
 
-<<<<<<< HEAD:src/main/java/de/rubicon/core/ListenerManager.java
-import de.rubicon.listener.CommandListener;
 import de.rubicon.listener.GuildJoinListener;
 import de.rubicon.listener.GuildMemberJoinListener;
-import de.rubicon.listener.SelfMentionListener;
-=======
 import fun.rubicon.listener.CommandListener;
 import fun.rubicon.listener.SelfMentionListener;
->>>>>>> 37a9dbf4ca22449da21f9ea36a6084eef96903ae:src/main/java/fun/rubicon/core/ListenerManager.java
 import net.dv8tion.jda.core.JDABuilder;
 
 public class ListenerManager {
