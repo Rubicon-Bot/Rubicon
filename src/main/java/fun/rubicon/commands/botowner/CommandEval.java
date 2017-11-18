170e3229df2776511b7a47b0174d5d86923f8f7a
リー <meluten@gmail.com>
2017-11-18 20:25:36 +0100

    Eval Command

---
+++ src/main/java/fun/rubicon/commands/botowner/CommandEval.java
@@ -0,0 +1,99 @@
+package fun.rubicon.commands.botowner;
+
+import fun.rubicon.command.Command;
+import fun.rubicon.command.CommandCategory;
+import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/**
+ * Amme JDA BOT
+ * <p>
+ * By LordLee at 18.11.2017 20:13
+ * <p>
+ * Contributors for this class:
+ * - github.com/zekrotja
+ * - github.com/DRSchlaubi
+ * <p>
+ * © Coders Place 2017
+ */
+public class CommandEval extends Command{
+    public CommandEval(String command, CommandCategory category) {
+        super(command, category);
+    }
+
+    @Override
+    protected void execute(String[] args, MessageReceivedEvent e) {
+
+        String[] par = String.join(" ", args).split("\\s+", 2);
+
+        ScriptEngine se = new ScriptEngineManager().getEngineByName("Nashorn");
+        try {
+            se.eval("var imports = new JavaImporter(" +
+                    "java.nio.file," +
+                    "Packages.net.dv8tion.jda.core.Permission," +
+                    "Packages.net.dv8tion.jda.core," +
+                    "java.lang," +
+                    "java.lang.management," +
+                    "java.text," +
+                    "java.sql," +
+                    "java.util," +
+                    "java.time," +
+                    "Packages.com.sun.management" +
+                    ");");
+        } catch (ScriptException er) {
+            er.printStackTrace();
+        }
+        se.put("event", e);
+        se.put("jda", e.getJDA());
+        se.put("guild", e.getGuild());
+        se.put("channel", e.getChannel());
+        se.put("message", e.getMessage());
+        se.put("author", e.getAuthor());
+
+        String modified_msg = args[0]
+                .replace("getToken", "getTextChannelById(channel.getId()).sendMessage(\"UnsupportedOperationException(\\\"Nice try m8!\\\")\").queue");
+        //        .replace("ProcessBuilder","throw new UnsupportedOperationException(\"Locked\")");
+
+        //    modified_msg = modified_msg.replaceAll("#", "().");
+
+        try {
+            Object out = se.eval(
+                    "{" +
+                            "with (imports) {" +
+                            modified_msg +
+                            "}" +
+                            "};");
+
+            if (out == null) {
+                out = "Your action..";
+            }
+
+            e.getChannel().sendMessage(new StringBuilder().append("```Java\n").append(modified_msg)
+                    .append("```Evaluated successfully:").toString()).queue();
+            new MessageBuilder().appendCodeBlock(out.toString(), "Java").buildAll(MessageBuilder.SplitPolicy.NEWLINE, MessageBuilder.SplitPolicy.SPACE, MessageBuilder.SplitPolicy.ANYWHERE).forEach(message -> e.getTextChannel().sendMessage(message).queue());
+        } catch (ScriptException er) {
+            e.getTextChannel().sendMessage(new StringBuilder().append("```Java\n").append(modified_msg)
+                    .append("```An exception was thrown:").toString()).queue();
+            new MessageBuilder().appendCodeBlock(e.toString(), "Java").buildAll(MessageBuilder.SplitPolicy.NEWLINE, MessageBuilder.SplitPolicy.SPACE, MessageBuilder.SplitPolicy.ANYWHERE).forEach(message -> e.getTextChannel().sendMessage(message).queue());
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        return "Simple Evealation Command!";
+    }
+
+    @Override
+    public String getUsage() {
+        return "eval <CODE>";
+    }
+
+    @Override
+    public int getPermissionLevel() {
+        return 4;
+    }
+}
