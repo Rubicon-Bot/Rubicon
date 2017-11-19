a6c032ffc93943aad758b92ff98c722770c69373
Michael R.|SchlaumeierTV <michael@rittmeister.in>
2017-11-19 20:21:43 +0100

    Short command
    - Added short command

--- src/main/java/fun/rubicon/core/CommandManager.java
+++ src/main/java/fun/rubicon/core/CommandManager.java
@@ -46,11 +46,10 @@ public class CommandManager {
         CommandHandler.addCommand(new CommandChoose("choose", CommandCategory.FUN));
         CommandHandler.addCommand(new CommandLevel("rank", CommandCategory.FUN).addAliases("lvl"));
         CommandHandler.addCommand(new CommandBday("birthday", CommandCategory.FUN).addAliases("bday"));
-<<<<<<< HEAD
         CommandHandler.addCommand(new CommandShort("short", CommandCategory.TOOLS));
-=======
         CommandHandler.addCommand(new CommandPortal("portal", CommandCategory.FUN).addAliases("mirror"));
->>>>>>> 75f1d3aff4a47aad4f1e2bc35f427449d2bd69fd
+        CommandHandler.addCommand(new CommandShort("short", CommandCategory.FUN));
+
 
         //general
         CommandHandler.addCommand(new CommandHelp("help", CommandCategory.GENERAL).addAliases("h"));
