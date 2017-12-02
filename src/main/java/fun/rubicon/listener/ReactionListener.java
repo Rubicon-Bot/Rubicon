4747f5b3f3dd33fa3d8bdb37aed6a4c31c7dcb30
Michael R.|SchlaumeierTV <mail@schlaubi.me>
2017-12-02 15:59:43 +0100

    Bugfix

--- src/main/java/fun/rubicon/listener/ReactionListener.java
+++ src/main/java/fun/rubicon/listener/ReactionListener.java
@@ -1,11 +1,7 @@
 package fun.rubicon.listener;
 
-<<<<<<< HEAD
 import fun.rubicon.commands.admin.CommandAutochannel;
-import fun.rubicon.commands.admin.CommandGivaway;
-=======
 import fun.rubicon.commands.admin.CommandGiveaway;
->>>>>>> master
 import fun.rubicon.commands.botowner.CommandBroadcast;
 import fun.rubicon.commands.tools.CommandVote;
 import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
