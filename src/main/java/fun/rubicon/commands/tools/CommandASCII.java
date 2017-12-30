4cfba245db9199e6a0fad58cefd0d193d963bcd3
ForYaSee <yannisee@web.de>
2017-12-30 20:36:01 +0100

    Ascii

--- src/main/java/fun/rubicon/commands/tools/CommandASCII.java
+++ src/main/java/fun/rubicon/commands/tools/CommandASCII.java
@@ -23,36 +23,33 @@ import net.dv8tion.jda.core.entities.Message;
 public class CommandASCII extends CommandHandler {
 
     public CommandASCII() {
-        super(new String[]{"ascii"}, CommandCategory.TOOLS, new PermissionRequirements(PermissionLevel.EVERYONE, "command.ascii"), "Convert an text to ASCII-Code", "<string>");
+        super(new String[]{"ascii"}, CommandCategory.TOOLS, new PermissionRequirements(PermissionLevel.EVERYONE, "command.ascii"), "Convert an Text to ASCII-Code", "<string>");
     }
 
     @Override
     protected Message execute(CommandManager.ParsedCommandInvocation parsedCommandInvocation, UserPermissions userPermissions) {
-        if (parsedCommandInvocation.args.length > 0) {
-            StringBuilder output = new StringBuilder();
-            StringBuilder text = new StringBuilder("Text: `");
-            StringBuilder asciiBuilder = new StringBuilder("ASCII-Code: `");
-            for (String arg : parsedCommandInvocation.args) {
-                text.append(arg).append(" ");
-                char[] chars = arg.toCharArray();
-                for (char c : chars) {
-                    String ascii = Integer.toString((int) c);
-                    if (ascii.length() == 1)
-                        asciiBuilder.append("00");
-                    else if (ascii.length() == 2)
-                        asciiBuilder.append("0");
-                    asciiBuilder.append(ascii).append(" ");
-                }
-                asciiBuilder.append("023 ");
+        StringBuilder output = new StringBuilder();
+        StringBuilder text = new StringBuilder("Text: `");
+        StringBuilder asciiBuilder = new StringBuilder("ASCII-Code: `");
+        for (String arg : parsedCommandInvocation.args) {
+            text.append(arg).append(" ");
+            char[] chars = arg.toCharArray();
+            for (char c : chars) {
+                String ascii = Integer.toString((int) c);
+                if (ascii.length() == 1)
+                    asciiBuilder.append("00");
+                else if (ascii.length() == 2)
+                    asciiBuilder.append("0");
+                asciiBuilder.append(ascii).append(" ");
             }
-            output.append(text.toString()).append("`\n");
-            output.append(asciiBuilder.toString()).append("`");
-            EmbedBuilder builder = new EmbedBuilder()
-                    .setDescription(output.toString())
-                    .setColor(Colors.COLOR_PRIMARY);
-            parsedCommandInvocation.invocationMessage.getTextChannel().sendMessage(builder.build()).queue();
-        } else
-            return new MessageBuilder().setEmbed(EmbedUtil.error("", getParameterUsage()).build()).build();
+            asciiBuilder.append("023 ");
+        }
+        output.append(text.toString()).append("`\n");
+        output.append(asciiBuilder.toString()).append("`");
+        EmbedBuilder builder = new EmbedBuilder()
+                .setDescription(output.toString())
+                .setColor(Colors.COLOR_PRIMARY);
+        parsedCommandInvocation.invocationMessage.getTextChannel().sendMessage(builder.build()).queue();
         return null;
     }
 }
