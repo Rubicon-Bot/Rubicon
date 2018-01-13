391fb799c2c3452e67c3548623f00cf101427c3a
ForYaSee <yannisee@web.de>
2018-01-13 21:43:10 +0100

    .

--- src/main/java/fun/rubicon/core/BotPermissionChecker.java
+++ src/main/java/fun/rubicon/core/BotPermissionChecker.java
@@ -1,36 +1,30 @@
 package fun.rubicon.core;
 
-import fun.rubicon.RubiconBot;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Guild;
-import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.TextChannel;
-import net.dv8tion.jda.core.entities.User;
 
 /**
  * @author Yannick Seeger / ForYaSee
  */
 public abstract class BotPermissionChecker {
 
-    private User botUser = RubiconBot.getJDA().getSelfUser();
-
     public boolean canRead(Guild guild, TextChannel textChannel) {
-        Member bot = guild.getMember(botUser);
-        if (bot.getPermissions(textChannel).contains(Permission.MESSAGE_READ))
+        if (guild.getSelfMember().hasPermission(textChannel, Permission.MESSAGE_READ))
             return true;
         return false;
     }
 
     public boolean canWrite(Guild guild, TextChannel textChannel) {
-        Member bot = guild.getMember(botUser);
-        if (bot.getPermissions(textChannel).contains(Permission.MESSAGE_WRITE))
+        if (!canRead(guild, textChannel))
+            return false;
+        if (guild.getSelfMember().hasPermission(textChannel, Permission.MESSAGE_WRITE))
             return true;
         return false;
     }
 
     public boolean canManageMessage(Guild guild, TextChannel textChannel) {
-        Member bot = guild.getMember(botUser);
-        if(bot.getPermissions(textChannel).contains(Permission.MESSAGE_MANAGE))
+        if (guild.getSelfMember().hasPermission(textChannel, Permission.MESSAGE_MANAGE))
             return true;
         return false;
     }
