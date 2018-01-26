6d516934d77bbed6f2b240ca144a91d7305e9f15
Michael R.|SchlaumeierTV <mail@schlaubi.me>
2018-01-26 17:00:35 +0100

    Unban command & minor updates
    Added:
      - Addes unban command
    Changed:
      - Added useless reactions in SelfMentionListener.java
      - Bot now sends DO_NOT_DISTRUB while starting

---
+++ src/main/java/fun/rubicon/commands/moderation/CommandUnban.java
@@ -0,0 +1,49 @@
+package fun.rubicon.commands.moderation;
+
+import fun.rubicon.RubiconBot;
+import fun.rubicon.command.CommandCategory;
+import fun.rubicon.command.CommandHandler;
+import fun.rubicon.command.CommandManager;
+import fun.rubicon.data.PermissionLevel;
+import fun.rubicon.data.PermissionRequirements;
+import fun.rubicon.data.UserPermissions;
+import fun.rubicon.util.EmbedUtil;
+import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.exceptions.PermissionException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class CommandUnban extends CommandHandler{
+
+
+    public CommandUnban() {
+        super(new String[] {"unban"}, CommandCategory.MODERATION, new PermissionRequirements(PermissionLevel.WITH_PERMISSION, "command.unban"), "Unban", "unban <id>", false);
+    }
+
+
+    @Override
+    protected Message execute(CommandManager.ParsedCommandInvocation parsedCommandInvocation, UserPermissions userPermissions) {
+        String[] args = parsedCommandInvocation.getArgs();
+        Guild guild = parsedCommandInvocation.getGuild();
+        if(args.length == 0)
+            return createHelpMessage();
+        HashMap<String, Guild.Ban> banned_users_IDS = new HashMap<>();
+        guild.getBanList().complete().forEach(b -> {
+            banned_users_IDS.put(b.getUser().getId(), b);
+        });
+        if(!banned_users_IDS.containsKey(args[0]))
+            return new MessageBuilder().setEmbed(EmbedUtil.error("User not banned", "This user is not banned").build()).build();
+        User user = banned_users_IDS.get(args[0]).getUser();
+        try {
+            guild.getController().unban(user).queue();
+        } catch (PermissionException e){
+            return new MessageBuilder().setEmbed(EmbedUtil.error("No permission", "Unable to unban user").build()).build();
+        }
+        return new MessageBuilder().setEmbed(EmbedUtil.success("User unbanned", "Succesfully unbanned " + user.getAsMention()).build()).build();
+    }
+}
