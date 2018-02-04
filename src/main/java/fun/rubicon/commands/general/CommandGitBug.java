d5eb2c698c0cfa0b88cf201064a5d6b69b8bf3fc
Leon Kappes <meluten@gmail.com>
2018-02-04 17:29:30 +0100

    Reformat GitBug

--- src/main/java/fun/rubicon/commands/general/CommandGitBug.java
+++ src/main/java/fun/rubicon/commands/general/CommandGitBug.java
@@ -34,25 +34,26 @@ import java.util.concurrent.TimeUnit;
  * @license MIT License <http://rubicon.fun/license>
  * @package fun.rubicon.commands.general
  */
-public class CommandGitBug extends CommandHandler{
-    public static HashMap<TextChannel,Titel> channelMsg = new HashMap<>();
+public class CommandGitBug extends CommandHandler {
+    public static HashMap<TextChannel, Titel> channelMsg = new HashMap<>();
 
     public CommandGitBug() {
-        super(new String[]{"gitbug"},CommandCategory.GENERAL,new PermissionRequirements(PermissionLevel.EVERYONE,"command.gitbug"),"Report an Bug","<Bug title>");
+        super(new String[]{"gitbug"}, CommandCategory.GENERAL, new PermissionRequirements(PermissionLevel.EVERYONE, "command.gitbug"), "Report an Bug", "<Bug title>");
     }
+
     private static String Header = "## Issue<br><br>### Issue Type<br>- [x] Bug.<br>- [ ] Feature.<br><br><br><br>### Report<br><br>#### Description<br><br>";
 
     @Override
     protected Message execute(CommandManager.ParsedCommandInvocation parsedCommandInvocation, UserPermissions userPermissions) {
         String title = parsedCommandInvocation.getMessage().getContentDisplay().replace(parsedCommandInvocation.getPrefix() + parsedCommandInvocation.getCommandInvocation(), "");
-        Titel  tite1 = new Titel(title, parsedCommandInvocation.getAuthor(), parsedCommandInvocation.getTextChannel());
-        channelMsg.put(parsedCommandInvocation.getTextChannel(),tite1);
-        SafeMessage.sendMessage(parsedCommandInvocation.getTextChannel(),new EmbedBuilder().setTitle("Set Bug Description").setDescription("Please write a short Description about the Bog in this Channel").setFooter("Will abort in 30sec.",null).build(),30);
+        Titel tite1 = new Titel(title, parsedCommandInvocation.getAuthor(), parsedCommandInvocation.getTextChannel());
+        channelMsg.put(parsedCommandInvocation.getTextChannel(), tite1);
+        SafeMessage.sendMessage(parsedCommandInvocation.getTextChannel(), new EmbedBuilder().setTitle("Set Bug Description").setDescription("Please write a short Description about the Bog in this Channel").setFooter("Will abort in 30sec.", null).build(), 30);
 
-return null;
+        return null;
     }
 
-    public static void handle(MessageReceivedEvent event){
+    public static void handle(MessageReceivedEvent event) {
         if (!channelMsg.containsKey(event.getTextChannel()))
             return;
         if (event.getMessage().getContentDisplay().startsWith("rc!gitbug"))
@@ -67,29 +68,32 @@ return null;
             @Override
             public void run() {
                 channelMsg.remove(event.getTextChannel());
-                titel.getChannel().sendMessage("Setup abort").queue(message -> {message.delete().queueAfter(5L, TimeUnit.SECONDS);});
+                titel.getChannel().sendMessage("Setup abort").queue(message -> {
+                    message.delete().queueAfter(5L, TimeUnit.SECONDS);
+                });
                 return;
             }
         }, 30000);
         try {
             GitHub gitHub = GitHub.connectUsingOAuth(Info.GITHUB_TOKEN);
             GHRepository repository = gitHub.getOrganization("Rubicon-Bot").getRepository("Rubicon");
-            GHIssue Issue = repository.createIssue(titel.getTitle()).body(Header+event.getMessage().getContentDisplay()).label("Bug").label("Requires Testing").create();
+            GHIssue Issue = repository.createIssue(titel.getTitle()).body(Header + event.getMessage().getContentDisplay()).label("Bug").label("Requires Testing").create();
             channelMsg.remove(event.getTextChannel());
             event.getMessage().delete().queue();
-            SafeMessage.sendMessage(event.getTextChannel(),new EmbedBuilder().setTitle("Bug successfully send!").setDescription("Bug is available at: "+Issue.getHtmlUrl()).build(),20);
+            SafeMessage.sendMessage(event.getTextChannel(), new EmbedBuilder().setTitle("Bug successfully send!").setDescription("Bug is available at: " + Issue.getHtmlUrl()).build(), 20);
             timer.cancel();
         } catch (IOException e) {
             e.printStackTrace();
-    }}
+        }
+    }
 
-    private class Titel{
+    private class Titel {
         private final String title;
         private final User author;
         private final TextChannel channel;
 
 
-        private Titel(String title, User author, TextChannel channel){
+        private Titel(String title, User author, TextChannel channel) {
             this.title = title;
             this.author = author;
             this.channel = channel;
