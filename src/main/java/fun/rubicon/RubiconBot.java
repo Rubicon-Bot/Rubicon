ae64c89fa1358cf721278e85f50d4fd739870a2c
Leon Kappes <meluten@gmail.com>
2018-02-04 17:23:04 +0100

    Added Github Bug system

--- src/main/java/fun/rubicon/RubiconBot.java
+++ src/main/java/fun/rubicon/RubiconBot.java
@@ -49,7 +49,7 @@ import java.util.*;
  */
 public class RubiconBot {
     private static final SimpleDateFormat timeStampFormatter = new SimpleDateFormat("MM.dd.yyyy HH:mm:ss");
-    private static final String[] CONFIG_KEYS = {"token", "mysql_host", "mysql_port", "mysql_database", "mysql_password", "mysql_user", "bitlytoken", "dbl_token", "gip_token", "lucsoft_token", "twitterConsumerKey", "twitterConsumerSecret", "twitterAccessToken", "twitterAccessTokenSecret", "google_token", "musixmatch_key"};
+    private static final String[] CONFIG_KEYS = {"token", "mysql_host", "mysql_port", "mysql_database", "mysql_password", "mysql_user", "bitlytoken", "dbl_token", "gip_token", "lucsoft_token", "twitterConsumerKey", "twitterConsumerSecret", "twitterAccessToken", "twitterAccessTokenSecret", "google_token", "musixmatch_key","git_token"};
     private static final String dataFolder = "data/";
     private static WebpanelManager webpanelManager;
     private static RubiconBot instance;
@@ -253,7 +253,8 @@ public class RubiconBot {
                 new CommandProfile(),
                 new CommandBio(),
                 new CommandMiner(),
-                new CommandPremium()
+                new CommandPremium(),
+                new CommandGitBug()
         );
         // settings commands package
         commandManager.registerCommandHandlers(
