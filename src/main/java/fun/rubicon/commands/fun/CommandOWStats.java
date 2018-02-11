70e319581734656c0236cd44d39d538ed6662e79
Leon Kappes <meluten@gmail.com>
2018-02-11 16:06:32 +0100

    Threading

--- src/main/java/fun/rubicon/commands/fun/CommandOWStats.java
+++ src/main/java/fun/rubicon/commands/fun/CommandOWStats.java
@@ -15,6 +15,7 @@ import fun.rubicon.util.EmbedUtil;
 import fun.rubicon.util.SafeMessage;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Message;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -36,28 +37,53 @@ public class CommandOWStats extends CommandHandler {
     protected Message execute(CommandManager.ParsedCommandInvocation parsedCommandInvocation, UserPermissions userPermissions){
         if(parsedCommandInvocation.getArgs().length<2)
             return createHelpMessage();
-        try {
-        HttpRequest request = new HttpRequest("https://owapi.net/api/v3/u/"+ parsedCommandInvocation.getArgs()[1].replace("#","-")+"/stats");
-        request.setRequestHeader(new RequestHeader().addField("User-Agent","RubiconBot"));
-            RequestResponse response = request.sendGETRequest();
-            JSONObject reg = (JSONObject) new JSONParser().parse(response.getResponse());
-            JSONObject plat = (JSONObject) reg.get(parsedCommandInvocation.getArgs()[0]);
-            JSONObject root = (JSONObject) plat.get("stats");
-            JSONObject quick = (JSONObject) root.get("quickplay");
-            JSONObject ranked = (JSONObject) root.get("competitive");
-
-
-            EmbedBuilder builder = new EmbedBuilder()
-                    .setAuthor((String)root.get("name"),null,(String)root.get("icon"))
-                    .setDescription("**Quickplay:**\n    - KD :"+((JSONObject)quick.get("game_stats")).get("kpd")+"\n    - Won Games :"+((JSONObject)quick.get("overall_stats")).get("wins")+"\n    - Played Games :"+((JSONObject)quick.get("overall_stats")).get("games")+"\n**Ranked:**\n    - KD :"+((JSONObject)ranked.get("game_stats")).get("kpd")+"\n    - Won Games :"+((JSONObject)ranked.get("overall_stats")).get("wins")+"\n    - Played Games :"+((JSONObject)ranked.get("overall_stats")).get("games")).setColor(Colors.COLOR_SECONDARY);
-
-            SafeMessage.sendMessage(parsedCommandInvocation.getTextChannel(),builder.build(),40);
-        } catch (Exception e) {
-            e.printStackTrace();
-            SafeMessage.sendMessage(parsedCommandInvocation.getTextChannel(), EmbedUtil.error("Player not found!","Is everything correct?").build(),40);
-        }
+        new Ow_Thread("Owstats",parsedCommandInvocation).start();
 
 
         return null;
     }
+
+    public class Ow_Thread implements Runnable {
+        private Thread t;
+        private String threadName;
+        private CommandManager.ParsedCommandInvocation parsedCommandInvocation;
+
+        Ow_Thread(String name, CommandManager.ParsedCommandInvocation Invocation) {
+            threadName = name;
+            parsedCommandInvocation = Invocation;
+        }
+
+        @Override
+        public void run() {
+            try {
+                HttpRequest request = new HttpRequest("https://owapi.net/api/v3/u/"+ parsedCommandInvocation.getArgs()[1].replace("#","-")+"/stats");
+                request.setRequestHeader(new RequestHeader().addField("User-Agent","RubiconBot"));
+                RequestResponse response = request.sendGETRequest();
+                JSONObject reg = (JSONObject) new JSONParser().parse(response.getResponse());
+                JSONObject plat = (JSONObject) reg.get(parsedCommandInvocation.getArgs()[0]);
+                JSONObject root = (JSONObject) plat.get("stats");
+                JSONObject quick = (JSONObject) root.get("quickplay");
+                JSONObject ranked = (JSONObject) root.get("competitive");
+
+
+                EmbedBuilder builder = new EmbedBuilder()
+                        .setAuthor((String)root.get("name"),null,(String)root.get("icon"))
+                        .setDescription("**Quickplay:**\n    - KD :"+((JSONObject)quick.get("game_stats")).get("kpd")+"\n    - Won Games :"+((JSONObject)quick.get("overall_stats")).get("wins")+"\n    - Played Games :"+((JSONObject)quick.get("overall_stats")).get("games")+"\n**Ranked:**\n    - KD :"+((JSONObject)ranked.get("game_stats")).get("kpd")+"\n    - Won Games :"+((JSONObject)ranked.get("overall_stats")).get("wins")+"\n    - Played Games :"+((JSONObject)ranked.get("overall_stats")).get("games")).setColor(Colors.COLOR_SECONDARY);
+
+                SafeMessage.sendMessage(parsedCommandInvocation.getTextChannel(),builder.build(),40);
+            } catch (Exception e) {
+                e.printStackTrace();
+                SafeMessage.sendMessage(parsedCommandInvocation.getTextChannel(), EmbedUtil.error("Player not found!","Is everything correct?").build(),40);
+            }
+        }
+
+        public void start() {
+            if (t == null) {
+                t = new Thread(this, threadName);
+                t.start();
+            }
+        }
+
+    }
+
 }
