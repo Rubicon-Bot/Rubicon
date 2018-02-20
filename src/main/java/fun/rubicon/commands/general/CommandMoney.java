91d041dd700195b8a16dede6916b62bdc15d5493
Leon Kappes <meluten@gmail.com>
2018-02-20 19:41:46 +0100

    fix money

--- src/main/java/fun/rubicon/commands/general/CommandMoney.java
+++ src/main/java/fun/rubicon/commands/general/CommandMoney.java
@@ -14,7 +14,6 @@ import fun.rubicon.core.entities.RubiconUser;
 import fun.rubicon.permission.PermissionRequirements;
 import fun.rubicon.permission.UserPermissions;
 import fun.rubicon.util.Colors;
-import fun.rubicon.util.EmbedUtil;
 import fun.rubicon.util.SafeMessage;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.MessageBuilder;
@@ -82,7 +81,7 @@ public class CommandMoney extends CommandHandler {
                             }
                         }
                     } catch (NumberFormatException e) {
-                        SafeMessage.sendMessage(invocation.getTextChannel(), invocation.translate("command.money.give.numbertoobig"));
+                        SafeMessage.sendMessage(invocation.getTextChannel(), invocation.translate("command.money.give.numbertoobig").replace("%s","2.147.483.647"));
                         return null;
                     }
                 } else {
@@ -112,7 +111,7 @@ public class CommandMoney extends CommandHandler {
                         return null;
                     }
                 } else {
-                    SafeMessage.sendMessage(invocation.getTextChannel(),no_permissions().build());
+                    SafeMessage.sendMessage(invocation.getTextChannel(), no_permissions().build());
                     return null;
                 }
             case "add":
