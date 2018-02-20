8eb5b41bc31ad6024052af5d42bf52dab1d4b381
ForYaSee <yannisee@web.de>
2018-02-20 18:26:00 +0100

    Push

---
+++ src/main/java/fun/rubicon/listener/AllShardsLoadedEvent.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018  Rubicon Bot Development Team
+ * Licensed under the GPL-3.0 license.
+ * The full license text is available in the LICENSE file provided with this project.
+ */
+
+package fun.rubicon.listener;
+
+/**
+ * @author Yannick Seeger / ForYaSee
+ */
+public class AllShardsLoadedEvent {
+
+    public AllShardsLoadedEvent() {
+        call();
+    }
+
+    private void call() {
+        
+    }
+}
