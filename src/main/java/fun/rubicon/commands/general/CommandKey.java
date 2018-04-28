5051dd3d268fbdf3284ca2aa69043fd019842f30
Leon Kappes <meluten@gmail.com>
2018-04-28 19:21:56 +0200

    Cleaned Up Keys Command

--- src/main/java/fun/rubicon/commands/general/CommandKey.java
+++ src/main/java/fun/rubicon/commands/general/CommandKey.java
@@ -1,18 +1,11 @@
 package fun.rubicon.commands.general;
 
-import fun.rubicon.RubiconBot;
 import fun.rubicon.command.CommandCategory;
 import fun.rubicon.command.CommandHandler;
 import fun.rubicon.command.CommandManager;
 import fun.rubicon.permission.PermissionRequirements;
 import fun.rubicon.permission.UserPermissions;
-import fun.rubicon.util.EmbedUtil;
-import fun.rubicon.util.Logger;
 import net.dv8tion.jda.core.entities.Message;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.UUID;
 
 /**
  * @author Leon Kappes / Lee
@@ -28,7 +21,7 @@ public class CommandKey extends CommandHandler {
 
     @Override
     protected Message execute(CommandManager.ParsedCommandInvocation invocation, UserPermissions userPermissions) {
-        /*if (invocation.getArgs().length < 2)
+        if (invocation.getArgs().length < 2)
             return createHelpMessage(invocation);
 
         switch (invocation.getArgs()[0]) {
@@ -36,26 +29,14 @@ public class CommandKey extends CommandHandler {
             case "generate":
                 switch (invocation.getArgs()[1]) {
                     case "premium":
-                        String token = UUID.randomUUID().toString();
-                        System.out.println(token.length());
-                        try {
-                            PreparedStatement ps = RubiconBot.getMySQL().prepareStatement("INSERT INTO `keys` (uuid, gift) VALUES (?,?)");
-                            ps.setString(1,token);
-                            ps.setString(2,"premium");
-                            ps.execute();
-                        } catch (SQLException e) {
-                            Logger.error(e);
-                        }
-                        invocation.getAuthor().openPrivateChannel().queue(privateChannel -> {
-                            privateChannel.sendMessage(EmbedUtil.success("Token Successfully generated", String.format("Token: %d\nRedeemable with rc!key %d", token, token)).build()).queue();
-                        });
+                        
                         break;
                     default:
                         return createHelpMessage(invocation);
 
                 }
 
-        }*/
+        }
         return null;
     }
 
