ff22bcc5d936833ab6944554ad74f9452a06ff27
Michael R.|SchlaumeierTV <mail@schlaubi.me>
2018-05-04 15:12:47 +0200

    Anti Embed update
    - The bot does now automatically convert embeds to normal messages, when he have no permission for embeds

--- src/main/java/fun/rubicon/util/SafeMessage.java
+++ src/main/java/fun/rubicon/util/SafeMessage.java
@@ -19,13 +19,13 @@ import java.util.concurrent.TimeUnit;
 public class SafeMessage {
     public static void sendMessage(TextChannel textChannel, Message message) {
         if (hasPermissions(textChannel))
-            textChannel.sendMessage(message).queue();
+           send(textChannel, message);
     }
 
     public static void sendMessage(TextChannel textChannel, Message message, int deleteTime) {
         try {
             if (hasPermissions(textChannel))
-                textChannel.sendMessage(message).queue(msg -> msg.delete().queueAfter(deleteTime, TimeUnit.SECONDS));
+                send(textChannel, message);
         } catch (Exception e) {
             // Ignored
         }
@@ -35,52 +35,84 @@ public class SafeMessage {
 
     public static Message sendMessageBlocking(TextChannel textChannel, Message message) {
         if (hasPermissions(textChannel))
-            return textChannel.sendMessage(message).complete();
+            send(textChannel, message);
         return null;
     }
 
     public static void sendMessage(TextChannel textChannel, String message) {
         if (hasPermissions(textChannel))
-            textChannel.sendMessage(message).queue();
+            send(textChannel, new MessageBuilder().setContent(message).build());
     }
 
     public static void sendMessage(TextChannel textChannel, String message, int deleteTime) {
         if (hasPermissions(textChannel) && hasDeletePermission(textChannel))
-            textChannel.sendMessage(message).queue(msg -> msg.delete().queueAfter(deleteTime, TimeUnit.SECONDS));
+            send(textChannel, new MessageBuilder().setContent(message).build()).delete().queueAfter(deleteTime, TimeUnit.SECONDS);
         else if (hasPermissions(textChannel))
-            textChannel.sendMessage(message).queue();
+            send(textChannel, new MessageBuilder().setContent(message).build());
 
     }
 
     public static void sendMessage(TextChannel textChannel, MessageEmbed build) {
         if (hasPermissions(textChannel))
-            textChannel.sendMessage(build).queue();
+            send(textChannel, build);
     }
 
     public static void sendMessage(TextChannel textChannel, MessageEmbed build, int deleteTime) {
         if (hasPermissions(textChannel) && hasDeletePermission(textChannel))
-            textChannel.sendMessage(build).queue(msg -> msg.delete().queueAfter(deleteTime, TimeUnit.SECONDS));
+            send(textChannel, build).delete().queueAfter(deleteTime, TimeUnit.SECONDS);
         else if (hasPermissions(textChannel))
-            textChannel.sendMessage(build).queue();
+            send(textChannel, build);
     }
 
     public static Message sendMessageBlocking(TextChannel textChannel, String message) {
         if (hasPermissions(textChannel))
-            return textChannel.sendMessage(message).complete();
+            return send(textChannel, new MessageBuilder().setContent(message).build());
         return null;
     }
     public static Message sendMessageBlocking(TextChannel textChannel, MessageEmbed message) {
         if (hasPermissions(textChannel))
-            return textChannel.sendMessage(message).complete();
+            return send(textChannel, message);
         return null;
     }
 
 
     private static boolean hasPermissions(TextChannel channel) {
-        return channel.getGuild().getSelfMember().hasPermission(channel, Permission.MESSAGE_READ) && channel.getGuild().getSelfMember().hasPermission(channel, Permission.MESSAGE_WRITE);
+        return channel.getGuild().getSelfMember().hasPermission(channel, Permission.MESSAGE_READ);
     }
 
+    private static boolean hasEmbedPermissions(TextChannel channel){
+        return channel.getGuild().getSelfMember().hasPermission(channel, Permission.MESSAGE_EMBED_LINKS);
+    }
 
+    private static Message send(TextChannel textChannel, Message msg){
+        if(!hasEmbedPermissions(textChannel) && !msg.getEmbeds().isEmpty())
+            return textChannel.sendMessage(formatEmbed(msg.getEmbeds().get(0))).complete();
+        else
+            return textChannel.sendMessage(msg).complete();
+    }
+    private static Message send(TextChannel textChannel, MessageEmbed msg){
+        if(!hasEmbedPermissions(textChannel))
+            return textChannel.sendMessage(formatEmbed(msg)).complete();
+        else
+            return textChannel.sendMessage(msg).complete();
+    }
+
+    private static String formatEmbed(MessageEmbed embed){
+        StringBuilder string = new StringBuilder();
+        if(embed.getTitle() != null)
+            string.append("**__").append(embed.getTitle()).append("__**").append("\n");
+        if(embed.getDescription() != null)
+            string.append(embed.getDescription());
+        embed.getFields().forEach(field -> {
+            string.append("**__").append(field.getName()).append("__**\n").append(field.getValue()).append("\n");
+        });
+        if(embed.getFooter() != null)
+            string.append("_").append(embed.getFooter().getText()).append("_");
+        String out = string.toString();
+        if(string.length() > 1024)
+            out = "This message is to longer than 1024 chars, please give me `MESSAGE_EMBED_LINKS` permission and try again";
+        return out;
+    }
 
     private static boolean hasDeletePermission(TextChannel channel) {
         return channel.getGuild().getSelfMember().hasPermission(channel, Permission.MESSAGE_MANAGE);
